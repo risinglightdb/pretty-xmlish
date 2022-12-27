9af55e1e182d72e8ce35e7947f35d80141d37bbc
ice1000 <ice1000kotlin@foxmail.com>
2022-12-27 07:34:42 -0500

    unicode: add constants

--- src/unicode.rs
+++ src/unicode.rs
@@ -1,5 +1,11 @@
 use crate::{LinedBuffer, Pretty, PrettyConfig};
 
+// https://www.w3.org/TR/xml-entity-names/025.html
+const UP_RIGHT: char = '\u{2514}';
+const UP_RIGHT_DOWN: char = '\u{251C}';
+const LEFT_RIGHT: char = '\u{2500}';
+const UP_DOWN: char = '\u{2502}';
+
 impl<'a> Pretty<'a> {
     pub(crate) fn ol_len_unicode(&self) -> usize {
         use Pretty::*;
