59d81d3cb4b36e115dc95a5e626e28a6d7792d0c
ice1000 <ice1000kotlin@foxmail.com>
2022-12-27 07:37:27 -0500

    unicode: todo

--- src/unicode.rs
+++ src/unicode.rs
@@ -6,5 +6,21 @@ const UP_RIGHT_DOWN: char = '\u{251C}';
 const LEFT_RIGHT: char = '\u{2500}';
 const UP_DOWN: char = '\u{2502}';
 
-impl<'a> Pretty<'a> {
+impl PrettyConfig {
+    pub(crate) fn interesting_unicode(
+        &self,
+        base_indent: usize,
+        pretty: &Pretty,
+        additional: usize,
+    ) -> usize {
+        let first_line_base = base_indent + additional;
+        let len = pretty.ol_len() + first_line_base;
+        if len <= self.width {
+            len
+        } else {
+            let next_indent = base_indent + self.indent;
+            use Pretty::*;
+            todo!()
+        }
+    }
 }
